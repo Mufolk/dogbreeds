9c6cc52139a858e630591b7110566eca0575d902
gmelo138 <138888978+gmelo138@users.noreply.github.com>
2025-10-02 15:15:19 -0300

    adjusting ngix proxy config

--- frontend/start-nginx.sh
+++ frontend/start-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${API_BASE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${API_BASE_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
